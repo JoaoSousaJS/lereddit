c34fee9aeb7ee52ac1a77961e3dcf627aa1ee4f0
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-11 18:24:44 +1030

    feat: add findOne post function

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -27,6 +30,11 @@ let PostResolver = class PostResolver {
             return Post_1.Post.find();
         });
     }
+    post(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return Post_1.Post.findOne({ id });
+        });
+    }
 };
 __decorate([
     type_graphql_1.Query(() => [Post_1.Post]),
@@ -34,6 +42,13 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "posts", null);
+__decorate([
+    type_graphql_1.Query(() => Post_1.Post, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id', () => type_graphql_1.Int)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "post", null);
 PostResolver = __decorate([
     type_graphql_1.Resolver()
 ], PostResolver);
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Int, Query, Resolver } from 'type-graphql'
 import { Post } from '../database/entity/Post'
 
 @Resolver()
@@ -7,5 +7,11 @@ export class PostResolver {
   @Query(() => [Post])
   async posts (): Promise<Post[]> {
     return Post.find()
+  }
+
+  @Query(() => Post, { nullable: true })
+  async post (
+    @Arg('id', () => Int) id: number): Promise<Post | undefined> {
+    return Post.findOne({ id })
   }
 }
