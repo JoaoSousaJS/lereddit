e65a3e3101b3310200d19e2a38dd6a696e5e1419
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-13 20:35:25 +1030

    feat: add login resolver

--- src/resolvers/user/User.ts
+++ src/resolvers/user/User.ts
@@ -1,4 +1,4 @@
-import { Arg, Field, InputType, Mutation, Resolver } from 'type-graphql'
+import { Arg, Field, InputType, Mutation, ObjectType, Resolver } from 'type-graphql'
 import argon2 from 'argon2'
 import { User } from '../../database/entities/User'
 
@@ -9,6 +9,24 @@ class UsernamePasswordInput {
 
   @Field()
   password: string
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string
+
+  @Field()
+  message: string
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true })
+  user?: User
 }
 
 @Resolver()
@@ -24,5 +42,39 @@ export class UserResolver {
     await User.save(newUser)
 
     return newUser
+  }
+
+  @Mutation(() => UserResponse)
+  async login (
+    @Arg('options') options: UsernamePasswordInput): Promise<UserResponse> {
+    const user = await User.findOneOrFail({ username: options.username })
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'that username does not exist'
+          }
+        ]
+      }
+    }
+
+    const valid = await argon2.verify(user.password, options.password)
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'incorrect passwor'
+          }
+        ]
+      }
+    }
+
+    return {
+      user
+    }
   }
 }
